6e035b681c8532cea4dcf556bc4ad7f628566ee4
justynpride <justyn@book-in.me>
2021-09-04 16:23:04 +0100

    user

--- routes/web.php
+++ routes/web.php
@@ -260,6 +260,8 @@ Route::group(
             Route::get('/user/{id}/send-invitation-message',
                 [ManageAccountController::class, 'sendInvitationMessage']
             )->name('sendInvitationMessage');
+
+            Route::resource('users', 'UserController');
         });
 
         Route::get('select_organiser', [OrganiserController::class, 'showSelectOrganiser'])
