545ad78016ae87b9176e180a5cb78b936d82f86a
justynpride <justyn@book-in.me>
2021-09-22 17:49:30 +0100

    Update 2021_09_14_125632_create_groups_table.php

--- database/migrations/2021_09_14_125632_create_groups_table.php
+++ database/migrations/2021_09_14_125632_create_groups_table.php
@@ -18,7 +18,6 @@ class CreateGroupsTable extends Migration
             $table->timestamps();
             $table->string('name', 255)->default('');
             $table->string('town', 255)->default('');
-            $table->bigInteger('country_id')->nullable();
             $table->foreign('country_id')->references('id')->on('countries');
             $table->string('email')->default('');
             $table->bigInteger('organiser_id')->nullable();
